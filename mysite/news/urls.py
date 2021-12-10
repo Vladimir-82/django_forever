23cbac6751d0b6b94ecd6e267eef094926a545f6
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-12-10 15:06:59 +0300

    19 completed

--- mysite/news/urls.py
+++ mysite/news/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index), # при обращении к главной странице подключаем метод индекс
-    path('category/<int:category_id>/', get_category), # при обращении к КАТЕГОРИЯМ подключаем метод индекс
+    path('', index, name='home'), # при обращении к главной странице подключаем метод индекс
+    path('category+'
+         '.0/<int:category_id>/', get_category, name='category'), # при обращении к КАТЕГОРИЯМ подключаем метод индекс
 ]
