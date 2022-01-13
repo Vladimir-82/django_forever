11a748a4e7a8d8a30ce6d03ad45bfcdc1e6830c1
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2022-01-13 14:49:21 +0300

    delete and redact added (redact not correct)

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,9 +1,22 @@
 from django import forms
-from .models import Category
+from .models import News
 
 
-class NewsForm(forms.Form):
-    title = forms.CharField(max_length=150)
-    content = forms.CharField()
-    is_published = forms.BooleanField()
-    category = forms.ModelChoiceField(queryset=Category.objects.all())
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        # fields = '__all__'  # все поля
+        fields = ['title', 'content', 'is_published', 'category']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'category': forms.Select(attrs={'class': 'form-control'})
+        }
+
+
+# форма не связанная с моделью (не желательна к использованию)
+# class NewsForm(forms.Form):
+#     title = forms.CharField(max_length=150, label='Название:')
+#     content = forms.CharField(label='Текст:', required=False)   # required=False - делает поле не обязательным
+#     is_published = forms.BooleanField(label='Опубликовано?', initial=True)  # делает чекбокс по умолчанию отмеченным
+#     category = forms.ModelChoiceField(empty_label="Выберите категорию", label='Категория:', queryset=Category.objects.all())    # empty_label="Выберите категорию" изменяет заглавие выпадающего меню
--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,33 +1,101 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import ListView, DetailView, DeleteView, UpdateView
 
 from .models import News, Category
 from .forms import NewsForm
 
 
-def index(request):
-    news = News.objects.all()
-    context = {
-        'news': news,
-        'title': 'Список новостей',
-    }
-    return render(request, template_name='news/index.html', context=context)
+class HomeNews(ListView):
+    model = News  #аналог news = News.objects.all() в index
+    template_name = 'news/home_news_list.html'  # шаблон
+    context_object_name = 'news'  #имя переменной, которая отправляется в шаблон
+    # extra_context = {'title': 'Главная'}  # не рекомендуется но очень удобно для титульника вместо функции внизу
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
+    def get_queryset(self):
+        return News.objects.filter(is_published=True)  # покажет только новости с чекбоксами
 
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', {'news': news, 'category': category})
+class UpdateNews(UpdateView):
+    model = News
+    template_name = 'news/add_news.html'
+
+    form_class = NewsForm
+
+class DeleteNews(DeleteView):
+    model = News
+    template_name = 'news/delete_news.html'
+
+    success_url = '/'
 
 
-def view_news(request, news_id):
-    # news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {"news_item": news_item})
+
+class NewsByCategory(ListView):
+    model = News
+    template_name = 'news/home_news_list.html'
+    context_object_name = 'news' #имя переменной, которая отправляется в шаблон
+    allow_empty = False     #запрещаем показ пустых списков (не существующих) это исключает 500 ошибку при выборе и отдаст 404
+
+
+    def get_queryset(self):
+        return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
+        return context
+
+class ViewNews(DetailView):
+    model = News
+    context_object_name = 'news_item'  #имя переменной, которая отправляется в шаблон
+    # template_name = 'news/news_detail'
+    # pk_url_kwarg = 'news_id'
+
+# def index(request):
+#     news = News.objects.all()
+#     context = {
+#         'news': news,
+#         'title': 'Список новостей',
+#     }
+#     return render(request, template_name='news/index.html', context=context)
+
+
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     return render(request, 'news/category.html', {'news': news, 'category': category})
+
+
+
+
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {"news_item": news_item})
 
 
 def add_news(request):
     if request.method == 'POST':
-        pass
+        form = NewsForm(request.POST)
+        if form.is_valid():
+            news = form.save()
+            return redirect(news)  # переадресация после сохранения
     else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+        form = NewsForm()  #  создается пустая форма
+    return render(request, 'news/add_news.html', {'form': form})    #рисуем шаблон
+
+
+# не связанные формы
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             news = News.objects.create(**form.cleaned_data)  # сохраняет новость (в связаных формах - save())
+#             return redirect(news)  # переадресация после сохранения
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})
