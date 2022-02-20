6fb53e2f2d0701cf1682e3fd01a1f580bd8c1c2e
vladimir <grun_gespenst@tut.by>
2022-02-20 20:16:55 +0300

    changed

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, DeleteView, UpdateView
 
 from .models import News, Category
